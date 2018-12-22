d934f63d732b65f4a7a0e1a699853d81f6645702
Rodrigo <rcoto@protonmail.com>
2018-12-21 22:47:54 -0800

    Section 12, Lecture 115

---
+++ expensify-app/src/tests/actions/filters.test.js
@@ -0,0 +1,60 @@
+import {
+  setTextFilter,
+  sortByDate,
+  sortByAmount,
+  setStartDate,
+  setEndDate
+} from "../../actions/filters";
+import moment from "moment";
+
+test("should generate set start date action object", () => {
+  const action = setStartDate(moment(0));
+
+  expect(action).toEqual({
+    type: "SET_START_DATE",
+    startDate: moment(0)
+  });
+});
+
+test("should generate set end date action object", () => {
+  const action = setEndDate(moment(0));
+
+  expect(action).toEqual({
+    type: "SET_END_DATE",
+    endDate: moment(0)
+  });
+});
+
+test("should set clear out text filter", () => {
+  const action = setTextFilter();
+
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: ""
+  });
+});
+
+test("should set value for filter by text", () => {
+  const action = setTextFilter("test");
+
+  expect(action).toEqual({
+    type: "SET_TEXT_FILTER",
+    text: "test"
+  });
+});
+
+test("should set filter to sort by date", () => {
+  const action = sortByDate();
+
+  expect(action).toEqual({
+    type: "SORT_BY_DATE"
+  });
+});
+
+test("should set filter to sort by amount", () => {
+  const action = sortByAmount();
+
+  expect(action).toEqual({
+    type: "SORT_BY_AMOUNT"
+  });
+});

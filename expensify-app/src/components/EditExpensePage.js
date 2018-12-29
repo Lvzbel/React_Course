274da10b8c5078cc9988e0b45a1e7d483c38355f
Rodrigo <rcoto@protonmail.com>
2018-12-28 20:47:59 -0800

    Test: commit to save changes and detect a past error

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -4,25 +4,38 @@ import ExpenseForm from "./ExpenseForm";
 import { editExpense } from "../actions/expenses";
 import { removeExpense } from "../actions/expenses";
 
-const EditExpensePage = props => (
-  <div>
-    <ExpenseForm
-      expense={props.expense}
-      onSubmit={expense => {
-        props.dispatch(editExpense(props.expense.id, expense));
-        props.history.push("/");
-      }}
-    />
-    <button
-      onClick={() => {
-        props.dispatch(removeExpense({ id: props.expense.id }));
-        props.history.push("/")
-      }}
-    >
-      Remove
-    </button>
-  </div>
-);
+// Refactor EditExpensepage to be a class based component
+// Setup mapDispatchToProps editExpense and removeExpense
+
+// should render EditExpensePage
+// snapshot
+
+// should handle editExpense
+// spies
+
+// should handle removeExpense
+// spies
+
+export class EditExpensePage extends React.Component {
+  onSubmit = expense => {
+    this.props.editExpense(expense, expense)
+    this.props.history.push("/");
+  };
+
+  onClick = () => {
+    this.props.removeExpense(expense.id)
+    this.props.history.push("/");
+  };
+
+  render() {
+    return (
+      <div>
+        <ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+        <button onClick={this.onClick}>Remove</button>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, props) => {
   return {
@@ -32,4 +45,12 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-export default connect(mapStateToProps)(EditExpensePage);
+const mapDispatchToProps = dispatch => ({
+  editExpense: (expense => dispatch(editExpense(id, expense))),
+  removeExpense: (expense => dispatch(removeExpense({ id: expense.id })))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EditExpensePage);
